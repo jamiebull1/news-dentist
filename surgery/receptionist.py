253b518635e43ce8c47c1d7e506e394cef00bb13
Jamie Bull <jamie.bull@oco-carbon.com>
2016-02-26 08:48:34 +0100

    Add CSRF protection

--- surgery/receptionist.py
+++ surgery/receptionist.py
@@ -5,6 +5,7 @@ import os
 import random
 import sqlite3
 import time
+import uuid
 
 from flask import (g, session, request, render_template, abort, redirect, flash,
                    url_for, send_from_directory)
@@ -21,6 +22,16 @@ app = Flask(__name__, static_url_path='')
 app.config.from_object(keys)
 
 
+def generate_csrf_token():
+    if '_csrf_token' not in session:
+        csrf_token = str(uuid.uuid4())
+        session['_csrf_token'] = csrf_token
+    return session['_csrf_token']
+
+
+app.jinja_env.globals['csrf_token'] = generate_csrf_token   
+
+
 def init_db():
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
@@ -33,10 +44,17 @@ def connect_db():
 
 
 @app.before_request
+def csrf_protect():
+    if request.method == "POST":
+        token = session.pop('_csrf_token', None)
+        if not token or token != request.form.get('_csrf_token'):
+            abort(400)
+
+
+@app.before_request
 def before_request():
     g.db = connect_db()
-
-
+    
 @app.teardown_request
 def teardown_request(exception):
     db = getattr(g, 'db', None)
