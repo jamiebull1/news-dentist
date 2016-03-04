c5648e1c523158fd95b5fe4807c7eecbe2572e38
Jamie Bull <jamie.bull@oco-carbon.com>
2016-03-04 17:57:59 +0100

    Create the database if not present

--- surgery/receptionist.py
+++ surgery/receptionist.py
@@ -33,10 +33,13 @@ app.jinja_env.globals['csrf_token'] = generate_csrf_token
 
 
 def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+    try:
+        connect_db()
+    except:
+        with closing(connect_db()) as db:
+            with app.open_resource('schema.sql', mode='r') as f:
+                db.cursor().executescript(f.read())
+            db.commit()
 
 
 def connect_db():
